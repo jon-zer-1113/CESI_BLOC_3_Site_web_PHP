f5cfbf23c90060d3517c2f6ffd61c11daf6cdc59
Rafaelllllllll <mendes.rafael78@gmail.com>
2022-12-20 09:29:44 +0100

    transfert PC

---
+++ Validation/Validate.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace Validation;
+
+class Validate {
+    private $regExpUsername = "/^[A-Za-z0-9_-]*$/"; // only letters, numbers, hyphens and underscores allowed
+	private $regExpPassword = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d.*)(?=.*\W.*)[a-zA-Z0-9\S]{8,}$/";
+
+    // USER CONTENT
+    // Registration validation form
+    private function postUserRegistration($username, $firstname, $lastname, $email, $password)
+    {
+        $username = $_POST["username"];
+        $firstname = $_POST["firstname"];
+        $lastname = $_POST["lastname"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+    }
+
+    private function validationUserRegistration($username, $firstname, $lastname, $email, $password)
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($username) || empty($firstname) ||empty($lastname) || empty($email) || empty($password)) {
+                exit("Un ou plusieurs champs vide(s) ⚠️");
+            }
+            if (!preg_match($this->regExpUsername, $username) || !filter_var($email, FILTER_VALIDATE_EMAIL) || !preg_match($this->regExpPassword, $password)) {
+                exit("Email et/ou mot de passe invalide(s) ⚠️");
+            } else {
+                return true;
+            }
+        }
+    }
+
+    public function validationNewUserAccount($username, $firstname, $lastname, $email, $password)
+    {
+        $this->postUserRegistration($username, $firstname, $lastname, $email, $password);
+        $this->validationUserRegistration($username, $firstname, $lastname, $email, $password);
+    } 
+
+    // login validation form
+    private function postUserLogin($email, $password) {
+		$email = $_POST["email"]; 	
+		$password = $_POST["password"];
+	}
+
+	private function validationCurrentUserAccount($email, $password) {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {	
+            if (empty($email) || empty($password)) {
+                exit("Un ou plusieurs champ(s) vide(s) ⚠️");
+            }
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL) || !preg_match($this->regExpPassword, $password)) {
+                exit("Email et/ou mot de passe invalide(s) ⚠️");
+            } else {
+				return true;                                                                                                                                                                                                                           
+            }
+        }
+    }
+
+    public function validationUserAccount($username, $password)
+    {
+        $this->postUserLogin($username, $password);
+        $this->validationCurrentUserAccount($username, $password);
+    }
+
+    // New comment validation form
+    private function postNewComment($commentTitle, $content, $title, $username, $userId, $recipeId)
+    {
+        $commentTitle = $_POST["commentTitle"];
+        $content = $_POST["content"];
+        $title = $_POST["title"];
+        $username = $_POST["username"];
+        $userId = $_POST["userId"];
+        $userId = $_POST["recipeId"];
+    }
+
+    private function validationCurrentComment($commentTitle, $content, $title, $username, $userId, $recipeId)
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($commentTitle) || empty($content) || empty($title) || empty($username) || empty($userId) || empty($recipeId)) {
+                exit("Un ou plusieurs champ(s) vide(s) ⚠️");
+            } else {
+                return true;
+            }
+        }
+    }
+
+    public function validationNewComment($commentTitle, $content, $title, $username, $userId, $recipeId)
+    {
+        $this->postNewComment($commentTitle, $content, $title, $username, $userId, $recipeId);
+        $this->validationCurrentComment($commentTitle, $content, $title, $username, $userId, $recipeId);
+    }
+
+    // ADMIN CONTENT
+    // Registration validation form
+    private function postAdminRegistration($adminUsername, $adminFirstname, $adminLastname, $adminEmail, $adminPassword)
+    {
+        $username = $_POST["adminUsername"];
+        $firstname = $_POST["adminFirstname"];
+        $lastname = $_POST["adminLastname"];
+        $email = $_POST["adminEmail"];
+        $password = $_POST["adminPassword"];
+    }
+
+    private function validationAdminRegistration($adminUsername, $adminFirstname, $adminLastname, $adminEmail, $adminPassword)
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($adminUsername) || empty($adminFirstname) || empty($adminLastname) || empty($adminEmail) || empty($adminPassword)) {
+                exit("Un ou plusieurs champ(s) vide(s) ⚠️");
+            }
+            if (!preg_match($this->regExpUsername, $adminUsername) || !filter_var($adminEmail, FILTER_VALIDATE_EMAIL) || !preg_match($this->regExpPassword, $adminPassword)) {
+                exit("Email et/ou mot de passe invalide(s) ⚠️");
+            } else {
+                return true;
+            }
+        }
+    }
+
+    public function validationNewAdminAccount($adminUsername, $adminFirstname, $adminLastname, $adminEmail, $adminPassword)
+    {
+        $this->postAdminRegistration($adminUsername, $adminFirstname, $adminLastname, $adminEmail, $adminPassword);
+        $this->validationAdminRegistration($adminUsername, $adminFirstname, $adminLastname, $adminEmail, $adminPassword);
+    } 
+
+    // Login validation form
+    private function postAdminLogin($email, $password)
+    {
+        $email = $_POST["adminEmail"];
+        $password = $_POST["adminPassword"];
+    }
+
+    private function validationCurrentAdminAccount($email, $password)
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($email) || empty($password)) {
+                exit("Un ou plusieurs champ(s) vide(s) ⚠️");
+            }
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL) || !preg_match($this->regExpPassword, $password)) {
+                exit("Email et/ou mot de passe invalide(s) ⚠️");
+            } else {
+                return true;
+            }
+        }
+    }
+
+	public function validationAdminAccount($username,$password)
+	{
+		$this->postAdminLogin($username,$password);
+		$this->validationCurrentAdminAccount($username,$password);
+	} 
+    
+    // Update recipe validation
+    private function postRecipe($title, $description, $prepTime, $bakeTime, $totalTime, $difficulty, $cost, $ingredients, $steps, $adminUsername) {
+		$title = $_POST["title"];
+		$description = $_POST["description"]; 	 	
+		$prepTime = $_POST["prepTime"]; 	
+		$bakeTime = $_POST["bakeTime"];
+		$totalTime = $_POST["totalTime"]; 	 	
+		$difficulty = $_POST["difficulty"]; 	
+		$cost = $_POST["cost"];
+        $ingredients = $_POST["ingredients"];
+        $steps = $_POST["steps"];
+        $adminUsername = $_POST["adminUsername"]; 	
+	}
+
+	private function validationRecipe($title, $description, $prepTime, $bakeTime, $totalTime, $difficulty, $cost, $ingredients, $steps, $adminUsername) {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {	
+            if (empty($title) || empty($description) || empty($prepTime) || empty($bakeTime) || empty($totalTime) || empty($difficulty) || empty($cost) || empty($ingredients) || empty($steps) || empty($adminUsername)) {
+                exit("Un ou plusieurs champ(s) vide(s) ⚠️");
+            } else {
+				return true;
+            }
+        }
+    }
+
+    public function validationNewRecipe($title, $description, $prepTime, $bakeTime, $totalTime, $difficulty, $cost, $ingredients, $steps, $adminUsername) {
+        $this->postRecipe($title, $description, $prepTime, $bakeTime, $totalTime, $difficulty, $cost, $ingredients, $steps, $adminUsername);
+        $this->validationRecipe($title, $description, $prepTime, $bakeTime, $totalTime, $difficulty, $cost, $ingredients, $steps, $adminUsername);
+    }
+
+    // Update recipe validation
+    private function postUpdateRecipe($title, $description, $prepTime, $bakeTime, $totalTime, $difficulty, $cost, $ingredients, $steps, $recipeId)
+    {
+        $title = $_POST["title"];
+        $description = $_POST["description"];
+        $prepTime = $_POST["prepTime"];
+        $bakeTime = $_POST["bakeTime"];
+        $totalTime = $_POST["totalTime"];
+        $difficulty = $_POST["difficulty"];
+        $cost = $_POST["cost"];
+        $ingredients = $_POST["ingredients"];
+        $steps = $_POST["steps"];
+        $recipeId = $_POST["recipeId"];
+    }
+
+    // Login validation
+    private function validationUpdateCurrentRecipe($title, $description, $prepTime, $bakeTime, $totalTime, $difficulty, $cost, $ingredients, $steps, $recipeId)
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($title) || empty($description) || empty($prepTime) || empty($bakeTime) || empty($totalTime) || empty($difficulty) || empty($cost) || empty($ingredients) || empty($steps)|| empty($recipeId)) {
+                exit("Un ou plusieurs champ(s) vide(s) ⚠️");
+            } else {
+                return true;
+            }
+        }
+    }
+
+    public function validationUpdateRecipe($title, $description, $prepTime, $bakeTime, $totalTime, $difficulty, $cost, $ingredients, $steps, $recipeId)
+    {
+        $this->postUpdateRecipe($title, $description, $prepTime, $bakeTime, $totalTime, $difficulty, $cost, $ingredients, $steps , $recipeId);
+        $this->validationUpdateCurrentRecipe($title, $description, $prepTime, $bakeTime, $totalTime, $difficulty, $cost, $ingredients, $steps, $recipeId);
+    }
+}
--- views/pages/visitor/home.php
+++ views/pages/visitor/home.php
@@ -6,5 +6,38 @@
         <h2 class="mt-3 mb-5 col-8 mx-auto">Bla bla</h2>
         <h3>Rejoins le côté obscur</h3>
         <p class="mt-4 mb-5 mx-auto px-5">Un espace dédié pour créer tes recettes de burgers et préparer tes listes de courses !</p>
+    <article>
+        <h2 class="mt-3 mb-5 col-8 mx-auto">Des recettes de burgers pour les geeks</h2>
+        <h3>Consulte nos recettes de burgers maisons</h3>
+        <p class="mt-4 mb-5 mx-auto px-5">Toutes nos recettes, rien que pour vos papilles. 8-bit-Burger c'est le goût des bonnes choses.</p>
+        <a href="#" class="btn btn-danger mb-3 text-uppercase" title="Supprimer">Inscris-toi</a>
+    </article>
+</section>
+<section class="visitor2__home-page text-center">
+    <h1>Concept</h1>
+
+    <article>
+        <h2 class="mt-3 mb-5 col-8 mx-auto">Pourquoi ce site ?</h2>
+        <h3>Des burgers bien geeks</h3>
+        <p class="mt-4 mb-5 mx-auto px-5">La création de ce site, vient de l'envie de partager notre amour pour les burgers. Une vie sans burgers ne merite pas d'étre vécue. Ce doux mets qui fond en bouche et qui met les petits comme les grands dans un bonheur gustatif. Parce que nous avons tous besoins de recharger nos RAM.<br><br>
+        Ce n'est pas nous qui avons adoptés les burgers geek mais ce sont eux qui nous ont envoutés. De Harry potter à Gandalf, tout le monde est hunanime pour dire que 8-bit Burger est le futur. La princesse Leia aurait dit un jour : "Un burger, mon royaume pour un burger".</p>
+        <img src="medias/img/visitor/food.png" alt="food" class="picture-food">
+    </article>
+</section>
+<section class="visitor3__home-page text-center">
+    <h1>Notre burger phare</h1>
+
+    <article>
+        <h2 class="mt-3 mb-5 col-8 mx-auto">Découvrez la star des burgers</h2>
+        <div class="visitor-burger-flex">
+            <div>
+            <img src="medias/img/common/burger.png" alt="burger" class="picture-food2">
+            </div>
+            <div>
+            <h3>Le MC CESI</h3>
+            <p class="mt-4 mb-5 mx-auto px-5">Découvrez cette recette tout droit sorti de l'antre du geek...</p>
+            <a href="#" class="btn btn-danger mb-3 text-uppercase" title="Supprimer">Inscris-toi</a>
+            </div>
+        </div>
     </article>
 </section>
