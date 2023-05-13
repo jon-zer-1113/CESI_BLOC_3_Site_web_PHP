2ae96f2a565283ad41aa3dd1d9f6292b4d998dc6
Mélanie Roussy <98660812+kitaah@users.noreply.github.com>
2023-05-13 19:55:45 +0200

    Update Delete.php
    Prepared statements with bindValue().

--- models/Delete.php
+++ models/Delete.php
@@ -4,26 +4,24 @@ namespace Models;
 
 class Delete extends DbConnection
 {
-    private $data;
     private $sql;
-    private $query;
+    private $stmt;
     private $timeZone;
     private $errDate;
     private $errLog;
 
-    // ADMIN CONTENT
-    // DELETE A SPECIFIC RECIPE (CRUD)
     private function deleteCurrentRecipe($recipeId)
     {
         try {
-            $this->data = [$recipeId];
             $this->sql = "DELETE FROM recipe WHERE recipeId = ?";
-            $this->query = $this->getDbConn()->prepare($this->sql)->execute($this->data);
+            $this->stmt = $this->getDbConn()->prepare($this->sql);
+            $this->stmt->bindValue(1, $recipeId, \PDO::PARAM_INT);
+            $this->stmt->execute();
         } catch(\PDOException $e) {
             $this->timeZone = date_default_timezone_set('Europe/Paris');
             $this->errDate = date('d-m-Y 📅 H:i:s ⏰');
             $this->errLog = file_put_contents('logs/delete/errors.txt', $e . $this->errDate . PHP_EOL, FILE_APPEND);
-            exit("Quelque chose ne va pas ⚠️! Merci de lire le message suivant ! ➡️ " . $e->getMessage() . " ⛔"); // Display SQLSTATE (code + message) next to the "Something went wrong!"
+            exit("Something went wrong ⚠️! Please read the following message ! ➡️ " . $e->getMessage() . " ⛔");
         }
     }
 
@@ -32,18 +30,18 @@ class Delete extends DbConnection
         $this->deleteCurrentRecipe($recipeId);
     }
 
-    // DELETE A SPECIFIC USER
     private function deleteCurrentUser($userId)
     {
         try {
-            $this->data = [$userId];
             $this->sql = "DELETE FROM user WHERE userId = ?";
-            $this->query = $this->getDbConn()->prepare($this->sql)->execute($this->data);
+            $this->stmt = $this->getDbConn()->prepare($this->sql);
+            $this->stmt->bindValue(1, $userId, \PDO::PARAM_INT);
+            $this->stmt->execute();
         } catch (\PDOException $e) {
             $this->timeZone = date_default_timezone_set('Europe/Paris');
             $this->errDate = date('d-m-Y 📅 H:i:s ⏰');
             $this->errLog = file_put_contents('logs/delete/errors.txt', $e . $this->errDate . PHP_EOL, FILE_APPEND);
-            exit("Quelque chose ne va pas ⚠️! Merci de lire le message suivant ! ➡️ " . $e->getMessage() . " ⛔"); // Display SQLSTATE (code + message) next to the "Something went wrong!"
+            exit("Something went wrong ⚠️! Please read the following message ! ➡️ " . $e->getMessage() . " ⛔");
         }
     }
 
