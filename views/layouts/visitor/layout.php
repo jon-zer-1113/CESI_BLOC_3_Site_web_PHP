a2143c53c887b1cb9538d64d09b58f5943458b6f
Mélanie Roussy <98660812+kitaah@users.noreply.github.com>
2023-05-13 20:15:33 +0200

    Update layout.php
    Bootstrap and Font Awesome last versions included, variable $sessionCookie included.

--- views/layouts/visitor/layout.php
+++ views/layouts/visitor/layout.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$this->sessionCookie();
 $_SESSION = array();
 session_destroy();
 ?><!DOCTYPE html>
@@ -10,8 +11,8 @@ session_destroy();
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="robots" content="noindex">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/style.min.css?v=<?= htmlspecialchars(time()); ?>">
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
@@ -21,17 +22,16 @@ session_destroy();
 </head>
 
 <body>
-	<!-- Header with a responsive navbar (hamburger menu) -->
 	<header>
 		<nav class="navbar navbar-dark navbar-expand-md text-uppercase fixed-top px-4">
-			<a class="navbar-brand" href="index.php?p=accueil"><img src="medias/img/common/8-bit_burger_logo.png" alt="8-bit Burger logo"></a>
+			<a class="navbar-brand" href="index.php?p=accueil"><img src="medias/img/common/8-bit_burger_logo.png" alt="8-Bit Burger logo"></a>
 			<button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="navbar-collapse collapse" id="navbar">
 				<ul class="navbar-nav ms-auto">
 					<li class="nav-item"><a href="index.php?p=accueil" class="nav-link first-link">Accueil</a></li>
-					<li class="nav-item"><a href="index.php?p=equipe" class="nav-link px-md-3 px-lg-3 px-xl-3">Equipe</a></li>
+					<li class="nav-item"><a href="index.php?p=equipe" class="nav-link px-md-3 px-lg-3 px-xl-3">Équipe</a></li>
 					<li class="nav-item"><a href="index.php?p=contact" class="nav-link px-md-3 px-lg-3 px-xl-3">Contact</a></li>
 					<li class="nav-item"><a href="#" class="nav-link" data-bs-toggle="modal" data-bs-target="#user-connexion"><i class="fa-solid fa-user"></i></a></li>
 				</ul>
@@ -39,11 +39,10 @@ session_destroy();
 		</nav>
 	</header>
 
-	<!-- The right view is displayed -->
 	<main id="visitor-top">
 		<?php require_once $this->visitorView; ?>
 		<div id="user-connexion" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="user-login__modal" aria-hidden="true">
-			<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-dialog modal-dialog-centered" role="document">
 				<div class="modal-content">
 					<div class="modal-header">
 						<h4 class="modal-title" id="user-login__modal"><i class="fa-solid fa-user pe-1"></i> Connexion</h4>
@@ -78,7 +77,7 @@ session_destroy();
 		</div>
 
 		<div id="user-registration" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="visitor-registration__modal" aria-hidden="true">
-			<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-dialog modal-dialog-centered" role="document">
 				<div class="modal-content">
 					<div class="modal-header">
 						<h4 class="modal-title" id="visitor-registration__modal"><i class="fa-solid fa-user-plus pe-1"></i> Inscription</h4>
@@ -132,12 +131,11 @@ session_destroy();
 		</div>
 	</main>
 
-	<!-- Footer with an anchor and a copyright mention -->
 	<footer class="text-center text-uppercase text-light">
-		<p><a href="#visitor-top"><i class="fa-solid fa-arrow-up pe-2"></i></a> © 8-bit burger - Tous droits réservés</p>
+		<p><a href="#visitor-top"><i class="fa-solid fa-arrow-up pe-2"></i></a> © 8-Bit Burger - Tous droits réservés</p>
 	</footer>
 
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 	<script src="js/app.min.js"></script>
 </body>
 
