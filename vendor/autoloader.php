a2edf24714b9867e91052dcd582a9a7583954340
Mélanie Roussy <98660812+kitaah@users.noreply.github.com>
2023-05-13 19:42:28 +0200

    Update autoloader.php
    Exit message changed.

--- vendor/autoloader.php
+++ vendor/autoloader.php
@@ -1,11 +1,10 @@
 <?php
-// The autoloader loads class files automatically 
 spl_autoload_register(function ($class) {
     $file = lcfirst(str_replace('\\', '/', $class)) . '.php';
     if (file_exists($file)) {
         require_once $file;
     } else {
-        exit("Le fichier {$class}.php n'a pas pu être trouvé ☹️!");
+        exit("The file {$file} could not be found ☹️!");
     }
 });
 
