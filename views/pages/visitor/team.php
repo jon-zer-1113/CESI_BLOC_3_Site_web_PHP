21827d7ad17f2c8c6586fd72e4901d823ff5ab9c
Mélanie Roussy <98660812+kitaah@users.noreply.github.com>
2023-05-14 15:26:07 +0200

    Update team.php

--- views/pages/visitor/team.php
+++ views/pages/visitor/team.php
@@ -1,6 +1,6 @@
 <?php ?>
 <section class="visitor__team-page text-center">
-    <h1>Equipe</h1>
+    <h1>Équipe</h1>
 
     <article>
         <h2 class="mt-3 mb-5 col-8 mx-auto">Une team super geek</h2>
@@ -12,14 +12,14 @@
                 <figure class="figure col-md-4">
                     <img src="medias/img/visitor/j-r_mendes_avatar.png" class="figure-img img-fluid rounded" alt="image de Jean-rafel Mendes">
                     <figcaption class="figure-caption col-8 mx-auto mt-1"><b class="text-uppercase">Jean-Rafael Mendes</b>
-                        <span>Celui-ci qui est toujours calme et assuré.</span>
+                        <span>Celui qui est toujours calme et assuré.</span>
                         <span><a href="https://www.linkedin.com/in/jean-rafael-mendes-988785b3/" class="text-light"><i class="fa-brands fa-linkedin mt-3 me-3"></i></a><a href="https://github.com/Rafael3378" class="text-light" target="_blank"><i class="fa-brands fa-github"></i></a></span>
                     </figcaption>
                 </figure>
                 <figure class="figure col-md-4 py-5 py-sm-5 py-md-0 y-lg-0 py-xl-0">
                     <img src="medias/img/visitor/melanie_roussy_avatar.png" class="figure-img img-fluid rounded" alt="Image de Mélanie Roussy">
                     <figcaption class="figure-caption col-8 mx-auto mt-1"><b class="text-uppercase">Mélanie Roussy</b>
-                        <span>Celle qui disparait pour coder longuement.</span>
+                        <span>Celle qui disparaît pour coder longuement.</span>
                         <span><a href="https://www.linkedin.com/in/m-roussy/" class="text-light"><i class="fa-brands fa-linkedin mt-3 me-3"></i></a><a href="https://github.com/kitaah" class="text-light" target="_blank"><i class="fa-brands fa-github"></i></a></span>
                     </figcaption>
                 </figure>
