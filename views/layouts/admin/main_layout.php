57d1f9c9799b26062c92737abf919a96bfa41689
Mélanie Roussy <98660812+kitaah@users.noreply.github.com>
2023-05-14 15:13:53 +0200

    Update main_layout.php
    Bootstrap and Font Awesome last versions included, variable $sessionCookie included.

--- views/layouts/admin/main_layout.php
+++ views/layouts/admin/main_layout.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$this->sessionCookie();
 $this->sessionOnlyAdmin();
 ?><!DOCTYPE html>
 <html lang="fr">
@@ -9,8 +10,8 @@ $this->sessionOnlyAdmin();
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="robots" content="noindex">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/style.min.css?v=<?= htmlspecialchars(time()); ?>">
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
@@ -20,7 +21,6 @@ $this->sessionOnlyAdmin();
 </head>
 
 <body>
-	<!-- Header with a responsive navbar (hamburger menu) -->
 	<header>
 		<nav class="navbar navbar-dark navbar-expand-md text-uppercase fixed-top px-4">
 			<a class="navbar-brand" href="index.php?p=accueil-admin-connexion"><img src="medias/img/common/8-bit_burger_logo.png" alt="8-bit Burger logo"></a>
@@ -39,10 +39,8 @@ $this->sessionOnlyAdmin();
 		</nav>
 	</header>
 
-	<!-- The right view is displayed -->
 	<main id="admin-top">
 		<?php require_once $this->adminView; ?>
-		<!-- MODAL: ADMIN INFORMATION ACCOUNT -->
 		<div id="admin-account-info" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="admin-account-info__modal" aria-hidden="true">
 			<div class="modal-dialog modal-dialog-centered" role="document">
 				<div class="modal-content">
@@ -63,12 +61,11 @@ $this->sessionOnlyAdmin();
 		</div>
 	</main>
 
-	<!-- Footer with an anchor and a copyright mention -->
 	<footer class="text-center text-uppercase text-light">
-		<p><a href="#admin-top"><i class="fa-solid fa-arrow-up pe-2"></i></a> © 8-bit burger - Tous droits réservés</p>
+		<p><a href="#admin-top"><i class="fa-solid fa-arrow-up pe-2"></i></a> © 8-Bit Burger - Tous droits réservés</p>
 	</footer>
 
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 	<script src="js/app.min.js"></script>
 </body>
 
