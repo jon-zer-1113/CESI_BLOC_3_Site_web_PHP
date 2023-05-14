a2247b49b45483d08af7d484568d620b7e27cf9f
Mélanie Roussy <98660812+kitaah@users.noreply.github.com>
2023-05-14 15:20:09 +0200

    Update error_404.php

--- views/pages/errors/error_404.php
+++ views/pages/errors/error_404.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php
+session_start();
+?><!DOCTYPE html>
 <html lang="fr">
 
 <head>
@@ -6,13 +8,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noindex">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link type="text/css" rel="stylesheet" href="css/style.min.css?v=<?= htmlspecialchars(time()); ?>">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://8bit-burger.melanieroussy.fr/css/style.min.css?v=<?= htmlspecialchars(time()); ?>">
+    <link rel=" apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <title>8-Bit Burger | Erreur 404</title>
+    <link rel="manifest" href="/manifest.json">
+    <title>8-Bit Burger | Erreur 403</title>
 </head>
 
 <body>
@@ -23,7 +25,7 @@
             <article>
                 <h2 class="mt-3 mb-5">Oups !</h2>
                 <p class="px-5">La page recherchée est introuvable.</p>
-                <img src="medias/img/errors/nyan_cat.gif" alt="Nyan cat gif">
+                <img src="https://8bit-burger.melanieroussy.fr/medias/img/errors/nyan_cat.gif" alt="Nyan cat gif">
             </article>
         </section>
     </main>
