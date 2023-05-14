c79ccb88e060336d6e2b139a6e40615a4a1fd68b
Mélanie Roussy <98660812+kitaah@users.noreply.github.com>
2023-05-14 15:25:47 +0200

    Update home.php

--- views/pages/visitor/home.php
+++ views/pages/visitor/home.php
@@ -1,12 +1,11 @@
 <?php ?>
-<!-- VISITOR HOME PAGE -->
 <section class="visitor__home-page text-center">
     <h1>Bienvenue chez <span>8-Bit Burger</span></h1>
 
     <article>
         <h2 class="mt-3 mb-5 col-8 mx-auto">Des recettes de burgers pour les geeks</h2>
         <h3>Découvre nos recettes <span>de burgers fait maison</span></h3>
-        <p class="mt-4 mb-4 mx-auto px-5">De superbes recettes fait maison, rien que pour tes papilles ! <span>8-Bit Burger c'est avant tout le goût des bonnes choses.</span></p>
+        <p class="mt-4 mb-4 mx-auto px-5">De superbes recettes fait maison, rien que pour tes papilles ! <span>8Bit-Burger c'est avant tout le goût des bonnes choses.</span></p>
         <a href="#" class="btn btn-danger text-uppercase" data-bs-toggle="modal" data-bs-target="#user-registration" title="Lance-toi !">Inscris-toi</a>
     </article>
 </section>
@@ -18,8 +17,8 @@
         <h3 class="mt-3 mb-5 col-8 mx-auto">En quoi ça consiste ?</h3>
         <p class="px-5">8-Bit Burger te permet de consulter des recettes de hamburgers, <span>sur fond de thème geek et rétro-futuriste.</span></p>
         <p class="col-md-6 px-5 mx-auto">Tu peux également laisser des commentaires sur les recettes, les télécharger et les partages par email avec tes amis !</p>
-        <p class="px-5">Tu peux aussi faire ta liste de courses et <span>la télécharger, pour réaliser la recette de ton choix.</span></p>
-        <p class="px-5">Chez les geeks, tout le monde est unanime, 8-Bit Burger c'est le meilleur !</p>
+        <p class="col-md-6 px-5 mx-auto">Tu peux aussi faire ta liste de courses pour réaliser la recette de ton choix.</span></p>
+        <p class="col-md-6 px-5 mx-auto">Chez les geeks, tout le monde est unanime, 8-Bit Burger c'est le meilleur !</p>
         <p class="col-md-6 px-5 mx-auto">La Princesse Leila aurait même dit un jour: "Un burger, mon royaume pour un burger !"</p>
         <img src="medias/img/visitor/food.png" alt="image de hamburgers et de frites">
     </article>
@@ -36,7 +35,7 @@
             </div>
             <div>
                 <h3>Le MC CESI</h3>
-                <p class="mt-4 mb-4 mx-auto px-5">Découvrez cette recette <span>tout droit sorti de l'antre du geek...</span></p>
+                <p class="mt-4 mb-4 mx-auto px-5">Découvrez cette recette <span>tout droit sortie de l'antre du geek...</span></p>
                 <a href="#" class="btn btn-danger mb-3 text-uppercase" data-bs-toggle="modal" data-bs-target="#user-registration" title="Lance-toi !">Inscris-toi</a>
             </div>
         </div>
