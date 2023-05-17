4a6b1fa949f80525417d3da619f6847c95bbd8ae
Mélanie Roussy <98660812+kitaah@users.noreply.github.com>
2023-05-17 13:00:53 +0200

    Update app.js
    Service worker registration integration.

--- js/app.js
+++ js/app.js
@@ -29,4 +29,16 @@ document.addEventListener("DOMContentLoaded", () => {
   })()
 });
 
+// SERVICE WORKER REGISTRATION
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => {
+      console.log('Service Worker registered');
+    })
+    .catch(error => {
+      console.error('Service Worker registration failed:', error);
+    });
+};
+
+
 
