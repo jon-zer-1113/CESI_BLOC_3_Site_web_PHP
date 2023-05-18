3332d71c0ee0045ad62e3ab8c64db2e63d02aba2
Mélanie Roussy <98660812+kitaah@users.noreply.github.com>
2023-05-18 21:58:12 +0200

    Update Create.php
    Useful comments for better understanding.

--- models/Create.php
+++ models/Create.php
@@ -10,7 +10,18 @@ class Create extends DbConnection
     private $timeZone;
     private $errDate;
     private $errLog;
-
+    
+     /**
+     * Creates a new user account in the database.
+     *
+     * @param string $username The username of the new user.
+     * @param string $firstname The first name of the new user.
+     * @param string $lastname The last name of the new user.
+     * @param string $email The email address of the new user.
+     * @param string $password The password of the new user.
+     *
+     * @throws \PDOException if a database error occurs during the insertion.
+     */
     private function newUserAccount($username, $firstname, $lastname, $email, $password)
     {
         try {
@@ -34,7 +45,18 @@ class Create extends DbConnection
     {
         $this->newUserAccount($username, $firstname, $lastname, $email, $password);
     } 
-
+    
+     /**
+     * Adds a new comment to the database.
+     *
+     * @param string $commentTitle The title of the comment.
+     * @param string $content The content of the comment.
+     * @param int $rating The rating associated with the comment.
+     * @param string $title The title associated with the comment.
+     * @param string $username The username of the commenter.
+     *
+     * @throws \PDOException if a database error occurs during the insertion.
+     */
     private function addNewComment($commentTitle, $content, $rating, $title, $username)
     {
         try {
@@ -58,7 +80,18 @@ class Create extends DbConnection
     {
         $this->addNewComment($commentTitle, $content, $rating, $title, $username);
     } 
-
+    
+    /**
+    * Creates a new admin account by inserting the admin details into the database.
+    *
+    * @param string $adminUsername The username of the admin.
+    * @param string $adminFirstname The first name of the admin.
+    * @param string $adminLastname The last name of the admin.
+    * @param string $adminEmail The email of the admin.
+    * @param string $adminPassword The password of the admin.
+    *
+    * @throws \PDOException if a database error occurs during the insertion.
+    */
     private function newAdminAccount($adminUsername, $adminFirstname, $adminLastname, $adminEmail, $adminPassword)
     {
         try {
@@ -82,7 +115,23 @@ class Create extends DbConnection
     {
         $this->newAdminAccount($adminUsername, $adminFirstname, $adminLastname, $adminEmail, $adminPassword);
     } 
-
+    
+    /**
+    * Adds a new recipe by inserting the recipe details into the database.
+    *
+    * @param string $title The title of the recipe.
+    * @param string $description The description of the recipe.
+    * @param int $prepTime The preparation time of the recipe (in minutes).
+    * @param int $bakeTime The baking time of the recipe (in minutes).
+    * @param int $totalTime The total time required for the recipe (in minutes).
+    * @param string $difficulty The difficulty level of the recipe.
+    * @param string $cost The cost of the recipe.
+    * @param string $ingredients The ingredients used in the recipe.
+    * @param string $steps The steps to follow for the recipe.
+    * @param string $adminUsername The username of the admin who adds the recipe.
+    *
+    * @throws \PDOException if a database error occurs during the insertion.
+    */
     private function addNewRecipe($title, $description, $prepTime, $bakeTime, $totalTime, $difficulty, $cost, $ingredients, $steps, $adminUsername)
     {
         try {
