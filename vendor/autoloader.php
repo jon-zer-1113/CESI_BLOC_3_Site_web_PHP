c5e4e8ed5f63e66816fa9f060b485ae13fdd1dd0
Mélanie Roussy <98660812+kitaah@users.noreply.github.com>
2023-05-18 21:06:01 +0200

    Update autoloader.php
    Useful comments for better understanding.

--- vendor/autoloader.php
+++ vendor/autoloader.php
@@ -1,5 +1,17 @@
 <?php
+
+/**
+ * Autoloader
+ * Registers an autoloader function that includes the necessary class files.
+ * Converts the namespace and class name to a file path and checks if the file exists.
+ * If found, includes the file. Otherwise, throws an error message.
+ *
+ * @param string $class The fully qualified class name
+ * @return void
+ */
+
 spl_autoload_register(function ($class) {
+    // Convert namespace and class name to a file path
     $file = lcfirst(str_replace('\\', '/', $class)) . '.php';
     if (file_exists($file)) {
         require_once $file;
@@ -7,4 +19,3 @@ spl_autoload_register(function ($class) {
         exit("The file {$file} could not be found ☹️!");
     }
 });
-
